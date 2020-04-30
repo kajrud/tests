4d2a1a8d15f69343c2b087145e6c5cae1af193c3
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-04-30 10:16:54 +0200

    30/04

---
+++ learning_unittests/twitter_test.py
@@ -0,0 +1,23 @@
+import unittest
+from learning_unittests import twitter
+
+
+class TwitterTest(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.twitter = twitter.Twitter()
+
+    def test_init(self):
+        self.assertTrue(self.twitter)
+
+    def test_tweet_single(self):
+        #Given
+        #sytuacja wejściowa, tutaj zapisana w setUp
+        #When
+        self.twitter.tweet("Test message")
+        #Then
+        self.assertEqual(self.twitter.tweets, ['Test message'])
+
+
+if __name__ == '__main__':
+    unittest.main()
