01168ea628acb69408c302d1dde5510c3c724633
kajrud <kaja.kinga.rudnicka@gmail.com>
2020-05-07 19:43:14 +0200

    07/05

--- learning_pytest/custom_test.py
+++ learning_pytest/custom_test.py
@@ -1,7 +1,11 @@
+import pytest
+import os
 
+@pytest.mark.skipif(os.environ.get('NO_SUMMING') == '1', reason='No summing set to 1')
 def test_sum():
     assert 2+2 == 4
 
+@pytest.mark.xfail
 def test_get_element_from_list():
     custom_list = ['test']
-    assert custom_list[0] == 'test'
+    assert custom_list[0] == 'fail'
